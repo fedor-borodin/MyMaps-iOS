8200eca10fed1b7b4ea64fc49693723f7aca471a
Admin <admin@Mac-Admin.local>
2017-05-08 13:37:23 +0300

    Initial Commit

---
+++ MyMaps-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMaps-iOS
+//
+//  Created by Admin on 08/05/2017.
+//  Copyright © 2017 fborodin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
